db2b676f2031e691134c4cb6f60107ea223f4172
reZerOR <gkraihan14k@gmail.com>
2023-12-22 12:50:01 +0600

    sever added

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// ready made middleware
+app.use(cors());
+app.use(express.json()) 
+
+
+
+// database
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4gj9vp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const taskCollection = client.db("toDoestDB").collection("tasks");
+
+    app.post('/tasks', async(req, res)=>{
+        const taskInfo = req.body
+        const result = await taskCollection.insertOne(taskInfo)
+        res.send(result)
+    })
+
+
+    app.get('/tasks', async(req,res)=>{
+        const email = req.query.email
+        const status = req.query.status
+        console.log(email)
+        let query = {email}
+        if (status){
+            query.status={status}
+        }
+        const result = await taskCollection.find(query).toArray()
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// root api
+app.get('/', (req, res)=>{
+    res.send('survey server is running')
+})
+
+// where the server port is
+app.listen(port, ()=>{
+    console.log(`survey is running on port: ${port}`)
+})
